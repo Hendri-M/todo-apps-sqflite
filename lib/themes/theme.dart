0b0d43027d2e72ead42ec1096b12eed6e86ed9e9
Hendri-M <hendrimuji31@gmail.com>
2024-05-09 04:22:41 +0700

    first commit

---
+++ lib/themes/theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color background = const Color(0xFF475C7A);
+Color fabColor = const Color(0xFFD8737F);
+Color contentColor = const Color(0xFFDDDDDD);
+Color formBgColor = const Color(0xFFEFEFEF);
+Color formFieldBgColor = const Color(0xFFFFFFFF);
+Color barrierColor = const Color(0xFFD9D9D9);
+
+// Text Color
+Color textColor = const Color(0xFF000000);
+Color hintColor = const Color(0xFF8A8A8A);
+Color cretedColor = const Color(0xFF696969);
+
+// Priority Colors
+Color prRed = const Color(0xFFE43131);
+Color prYellow = const Color(0xFFF5D323);
+Color prGreen = const Color(0xFF34F523);
+Color prOrange = const Color(0xFFF28332);
+
+// Fonts and Style
+TextStyle textStyle = GoogleFonts.mulish();
+
+FontWeight boldText = FontWeight.w700;
+FontWeight semiBoldText = FontWeight.w600;
+FontWeight regularText = FontWeight.w400;
